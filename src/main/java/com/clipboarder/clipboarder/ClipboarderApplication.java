91ea6d2cdab99c4e9414bbe7a19e7135e350ab49
UDADDY <gustmd5715@gmail.com>
2024-01-02 17:15:40 +0900

    feat: add spring boot skeleton code

---
+++ src/main/java/com/clipboarder/clipboarder/ClipboarderApplication.java
@@ -0,0 +1,13 @@
+package com.clipboarder.clipboarder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipboarderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipboarderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clipboarder/clipboarder/ClipboarderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clipboarder.clipboarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipboarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
