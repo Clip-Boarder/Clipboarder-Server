62c214298e0aa7b4e08d09be6302683e98878a45
UDADDY <gustmd5715@gmail.com>
2024-01-23 17:53:10 +0900

    conf: use jpa auditing to observe entity change

--- src/main/java/com/clipboarder/clipboarder/ClipboarderApplication.java
+++ src/main/java/com/clipboarder/clipboarder/ClipboarderApplication.java
@@ -2,8 +2,10 @@ package com.clipboarder.clipboarder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ClipboarderApplication {
 
 	public static void main(String[] args) {
