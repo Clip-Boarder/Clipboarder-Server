59d934ffc984491aff3360cdf9ec1948c0ccd153
UDADDY <gustmd5715@gmail.com>
2024-02-26 14:33:28 +0900

    feat: add signUp function for user

--- src/main/java/com/clipboarder/clipboarder/service/ClipboarderUserService.java
+++ src/main/java/com/clipboarder/clipboarder/service/ClipboarderUserService.java
@@ -1,10 +1,10 @@
 package com.clipboarder.clipboarder.service;
 
 import com.clipboarder.clipboarder.entity.ClipboarderUser;
-import com.clipboarder.clipboarder.entity.ClipboarderUserRole;
 import com.clipboarder.clipboarder.entity.dto.ClipboarderUserDTO;
 import com.clipboarder.clipboarder.repository.ClipboarderUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +12,24 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class ClipboarderService {
+public class ClipboarderUserService {
     private final ClipboarderUserRepository clipboarderUserRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void register(ClipboarderUserDTO clipboarderUserDTO){
+    public boolean signUp(ClipboarderUserDTO clipboarderUserDTO){
+        String email = clipboarderUserDTO.getEmail();
+
+        // 중복 확인
+        boolean isExist = clipboarderUserRepository.existsByEmail(email);
+        if(isExist)
+            return false;
+
+        // DB에 저장
         ClipboarderUser clipboarderUser = dtoToEntity(clipboarderUserDTO);
-        clipboarderUser.addRole(ClipboarderUserRole.USER);
-
         clipboarderUserRepository.save(clipboarderUser);
+
+        return true;
+
     }
 
     public ClipboarderUserDTO getUserByEmail(String email){
@@ -34,7 +43,6 @@ public class ClipboarderService {
     private ClipboarderUser dtoToEntity(ClipboarderUserDTO clipboarderUserDTO){
         ClipboarderUser clipboarderUser = ClipboarderUser.builder()
                 .email(clipboarderUserDTO.getEmail())
-                .password(passwordEncoder.encode(clipboarderUserDTO.getPassword()))
                 .name(clipboarderUserDTO.getName())
                 .picture(clipboarderUserDTO.getPicture())
                 .provider(clipboarderUserDTO.getProvider())
@@ -46,11 +54,10 @@ public class ClipboarderService {
     private ClipboarderUserDTO entityToDTO(ClipboarderUser clipboarderUser){
         ClipboarderUserDTO clipboarderUserDTO = ClipboarderUserDTO.builder()
                 .email(clipboarderUser.getEmail())
-                .password(clipboarderUser.getPassword())
                 .name(clipboarderUser.getName())
                 .picture(clipboarderUser.getPicture())
                 .provider(clipboarderUser.getProvider())
-                .roleSet(clipboarderUser.getRoleSet())
+                .role(clipboarderUser.getRole())
                 .build();
 
         return clipboarderUserDTO;
