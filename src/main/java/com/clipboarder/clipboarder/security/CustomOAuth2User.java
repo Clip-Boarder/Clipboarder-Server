deb99c5ddc040ba6b9df0795f0ae6669927a4322
UDADDY <gustmd5715@gmail.com>
2024-03-01 21:15:00 +0900

    feat: add customOAuth2User

---
+++ src/main/java/com/clipboarder/clipboarder/security/CustomOAuth2User.java
@@ -0,0 +1,38 @@
+package com.clipboarder.clipboarder.security;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+//    private Map<String, Object> attributes;
+//    private List<GrantedAuthority> authorities;
+    private String email;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return this.email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+}
